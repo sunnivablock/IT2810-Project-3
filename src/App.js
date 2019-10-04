524d72db7c2f6c759005b70a5c2172df6211de8b
sunnivbl <sunnivbl@stud.ntnu.no>
2019-10-04 09:38:54 +0200

    set up branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hællæ bællææ
+          Hey
         </p>
       </header>
     </div>
