5b918c6394b401872f4d546ba3766db64fb8dce6
Hanne Olsen <hannols@stud.ntnu.no>
2019-10-05 16:47:42 +0200

    Some functionality on sorting rows

--- src/components/table.js
+++ src/components/table.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
-import '../App.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 650,
-  },
-}));
 
-function createData(Digghetsbarometer, firstName, lastName, year) {
-  return { Digghetsbarometer, firstName, lastName, year };
+function createData(digghet, fornavn, etternavn, fodt) {
+  return { digghet, fornavn, etternavn, fodt};
 }
 
 const rows = [
@@ -32,33 +23,162 @@ const rows = [
   createData(10, "Chris", "Hemsworth", 1987),
 ];
 
-export default function SimpleTable() {
-  const classes = useStyles();
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const headCells = [
+  { id: 'digghet', numeric: true,  label: 'Digghetsfaktor' },
+  { id: 'fornavn', numeric: false,  label: 'Fornavn' },
+  { id: 'etternavn', numeric: false, label: 'Etternavn' },
+  { id: 'fodt', numeric: true,  label: 'Født' },
+  
+];
+
+function EnhancedTableHead(props) {
+  const { classes,  order, orderBy, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
 
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow className="row">
-            <TableCell>Digghet</TableCell>
-            <TableCell align="left">Fornavn</TableCell>
-            <TableCell align="left">Etternavn</TableCell>
-            <TableCell align="left">Født</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody className="subRow">
-          {rows.map(row => (
-            <TableRow key={row.Digghetsbarometer}>
-              <TableCell component="th" scope="row">
-                {row.Digghetsbarometer}
-              </TableCell>
-              <TableCell align="left">{row.firstName}</TableCell>
-              <TableCell align="left">{row.lastName}</TableCell>
-              <TableCell align="left">{row.year}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={order}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+};
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('desc');
+  const [orderBy, setOrderBy] = React.useState('digghet');
+  const [selected, setSelected] = React.useState([]);
+
+  const handleRequestSort = (event, property) => {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  };
+
+
+
+  const isSelected = name => selected.indexOf(name) !== -1;
+
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <div className={classes.tableWrapper}>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              
+            />
+            <TableBody>
+              {stableSort(rows, getSorting(order, orderBy))
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.name);
+
+                  return (
+                    <TableRow>
+                      <TableCell align="right">{row.digghet}</TableCell>
+                      <TableCell align="right">{row.fornavn}</TableCell>
+                      <TableCell align="right">{row.etternavn}</TableCell>
+                      <TableCell align="right">{row.fodt}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              
+            </TableBody>
+          </Table>
+        </div>
+      </Paper>
+    </div>
   );
 }
