aa610620fd54bfe67b29dc42c4e3ed7a3ae972dd
Frida <fridasoso@gmail.com>
2019-10-10 12:55:51 +0200

    mongoose functioning, simple queries

--- server/server.js
+++ server/server.js
@@ -1,21 +1,61 @@
 const express = require("express")
 const app = express()
-const mongodb = require('mongodb').MongoClient
-const url = "mongodb://it2810-09.idi.ntnu.no:27017/"
-const apiPort = 8080
+const mongoose = require('mongoose')
+const url = "mongodb://it2810-09.idi.ntnu.no:27017/Gutta"
+const apiPort = 8000
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
+//Set up default mongoose connection
+var mongoDB = url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', function(callback) {
+
+    //The code in this asynchronous callback block is executed after connecting to MongoDB. 
+    console.log('Successfully connected to MongoDB.');
+});
+
+var Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+var dataSchema = new Schema({
+    id: Number,
+    firstName: String,
+    lastName: String,
+    profession: String,
+    year: String,
+    rating: String
+  },
+  { timestamp: true, collection : 'Krutt' },
+);
+
+// export the new Schema so we could modify it using Node.js
+var actor = mongoose.model('actor', dataSchema);
+
+// find all persons who are actors
+/*actor.find({ 'profession': 'actor' }, function (err, actors) {
+  if (err) return handleError(err);
+  console.log(actors)
+  // 'actors' contains the list of persons that match the criteria.
+})*/
 
 app.get('/', function(req, res) {
-    mongodb.connect(url, function(err, db) {
-        if (err) throw err
-        let dbo = db.db("test")
-        dbo.collection("testdata").findOne({}, function(err, result) {
-            if (err) throw err
-            res.send(result)
-        })
-    })
-    
+    actor.find({ 'profession': 'actor' }, function (err, actors) {
+        if (err) return handleError(err);
+        console.log(actors)
+        // 'actors' contains the list of persons that match the criteria.
+      })
+      res.send("asda")
 })
-
-
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
