21cb07c13883bd6a33bf4fae7ac34f0e407e7c67
sunnivbl <sunnivbl@stud.ntnu.no>
2019-10-25 11:02:53 +0200

    #19 improved some tests

---
+++ sunnivbl@login.stud.ntnu.no/precache-manifest.574a6f84308131a1b9837e3a49a6b31e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aeb4def98f198ff90f2dd0d7300095",
+    "url": "/prosjekt3/index.html"
+  },
+  {
+    "revision": "3a752a1327d8bd2136ff",
+    "url": "/prosjekt3/static/css/main.4d496fed.chunk.css"
+  },
+  {
+    "revision": "12b815d6ae64431917c0",
+    "url": "/prosjekt3/static/js/2.cf5d885c.chunk.js"
+  },
+  {
+    "revision": "3a752a1327d8bd2136ff",
+    "url": "/prosjekt3/static/js/main.ea8c687c.chunk.js"
+  },
+  {
+    "revision": "530400ee5ea383dc8c1f",
+    "url": "/prosjekt3/static/js/runtime-main.f012d5cc.js"
+  },
+  {
+    "revision": "e679304d0951d4d375ca0a61caf35e5b",
+    "url": "/prosjekt3/static/media/brad.e679304d.png"
+  }
+]);
