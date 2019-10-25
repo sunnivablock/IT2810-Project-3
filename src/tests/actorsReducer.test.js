f5ed468b4acb4c6aad833df96d9ac3f9aa8404cc
Hanne Olsen <hannols@stud.ntnu.no>
2019-10-25 22:35:04 +0200

    #19-Added tests for actorsReducer

--- src/tests/actorsReducer.test.js
+++ src/tests/actorsReducer.test.js
@@ -1,5 +1,6 @@
 import {actorsReducer} from '../reducers/reducer'
-//import * as types from '../../constants/ActionTypes'
+import * as types from '../actions/index'
+
 
 describe('actorsReducer', () => {
   it('should return the initial state', () => {
@@ -12,4 +13,52 @@ describe('actorsReducer', () => {
             
       }
     )
-  })})
+  })
+})
+
+it('should handle FETCH_ACTORS_PENDING', () => {
+  expect(
+    actorsReducer([], {
+
+      type: types.FETCH_ACTORS_PENDING,
+      pending:true,
+      
+    })
+  ).toEqual(
+    {
+      pending:true
+    }
+  )
+})
+
+it('should handle FETCH_ACTORS_SUCCESS', () => {
+  expect(
+    actorsReducer([], {
+
+      type: types.FETCH_ACTORS_SUCCESS,
+      pending:false,
+      actors: actorsReducer.payload
+    })
+  ).toEqual(
+    {
+      pending: false,
+      actors: actorsReducer.payload
+    }
+  )
+})
+
+it('should handle FETCH_ACTORS_ERROR', () => {
+  expect(
+    actorsReducer([], {
+
+      type: types.FETCH_ACTORS_ERROR,
+      pending: false,
+      error: actorsReducer.error
+    })
+  ).toEqual(
+    {
+      pending: false,
+      error: undefined
+    }
+  )
+})
